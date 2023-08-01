1a95e13fafbf1df553c4f926f47ea6f0ff27611b
JATENCIA <atenciajaramilloj@gmail.com>
2023-08-01 08:39:10 -0500

    update

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,29 +1,32 @@
-import "./index.css";
-import App from "../App";
-import React from "react";
-import axios from "axios";
-import { store } from "./redux/Store/store";
-import { Provider } from "react-redux";
-import ReactDOM from "react-dom/client";
-import { Auth0Provider } from "@auth0/auth0-react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { persistStore } from "redux-persist";
+import './index.css'
+import App from '../App'
+import React from 'react'
+import axios from 'axios'
+import { store } from './redux/Store/store'
+import { Provider } from 'react-redux'
+import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth0-react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 
 // axios.defaults.baseURL = "http://localhost:5000";
-axios.defaults.baseURL = "https://sport-planet-staff.vercel.app/";
+axios.defaults.baseURL = 'https://sport-planet-staff.vercel.app/'
 // axios.defaults.baseURL = "https://sportsplanet-production.up.railway.app/";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+import { Auth0Provider } from '@auth0/auth0-react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <BrowserRouter>
       <Auth0Provider
-        domain="dev-st1uijgryxovpoys.us.auth0.com"
-        clientId="Mnhv7nkn8snaaR9XCwTda7Gb8QekVtQ3"
-        redirectUri={window.location.origin}
-      >
+        domain='dev-st1uijgryxovpoys.us.auth0.com'
+        clientId='Mnhv7nkn8snaaR9XCwTda7Gb8QekVtQ3'
+        authorizationParams={{
+          redirect_uri: window.location.origin
+        }}>
         <App />
       </Auth0Provider>
     </BrowserRouter>
   </Provider>
-);
+)
